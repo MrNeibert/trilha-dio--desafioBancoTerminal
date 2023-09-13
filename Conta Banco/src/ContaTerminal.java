f29a351c51f648f81516f5f6f6d8c99d54655f21
Willian <willian.neibertz@gmail.com>
2023-09-13 17:38:43 -0300

    Funcionalidade implementada

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -9,8 +9,25 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
-        	
+        System.out.println("Por favor, Insira os dados para criar sua conta(Numero de Conta, Agencia, seu Nome e Saldo). ");
+        System.out.print("Numero da Conta:");
+        numeroConta = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Agencia:");
+        agencia = sc.nextLine();
+        System.out.print("Nome Cliente:");
+        nomeCliente = sc.nextLine();
+        System.out.print("Saldo:");
+        saldo = sc.nextDouble();
 
-        System.out.println("Hello, World!");
+        String textoConcatenado ="Olá " + nomeCliente +
+         ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+         ", conta " + numeroConta +
+          " e seu saldo é de" + (saldo) +
+           " e já está disponível para saque!";
+        	
+        sc.close();
+
+        System.out.println(textoConcatenado);
     }
 }
