822c41f71a334cd6de48a84ca7a660dc95998daf
Willian <willian.neibertz@gmail.com>
2023-09-13 17:01:15 -0300

    inicio do projeto e inserção das variáveis

---
+++ Conta Banco/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        	
+
+        System.out.println("Hello, World!");
+    }
+}
